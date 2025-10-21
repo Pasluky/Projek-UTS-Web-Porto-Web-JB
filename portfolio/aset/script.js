fb3039500f64d168d3c763b7f455a95935b9d5d8
Lysandra <ilhamalfasluky@gmail.com>
2025-10-21 21:28:58 +0700

    Update script.js

--- portfolio/aset/script.js
+++ portfolio/aset/script.js
@@ -1,9 +1,3 @@
-function toggleMenu() {
-  const menu = document.getElementById('menu-utama');
-  if (!menu) return;
-  menu.classList.toggle('tampil');
-}
-
 function validasiForm(form) {
   alert('Terima kasih, pesan terkirim!');
   form.reset();
